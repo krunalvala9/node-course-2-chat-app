1db63d5c64469ca9ee66838b6d0909dd6f092fa8
Krunal Vala <krunalvala9@gmail.com>
2018-06-12 00:39:21 +0530

    Add join page that submits to chat.html

--- public/js/chat.js
+++ public/js/chat.js
@@ -12,7 +12,7 @@ function scrollToBottom () {
     var lastMessageHeight = newMessage.prev().innerHeight();
 
     if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
-        messages.scrollTop(scrollHeight);
+        messages.scrollTop(scrollHeight); 
     }
 }
 
