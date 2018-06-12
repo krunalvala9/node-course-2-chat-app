a6eda03c79f3edc15f17d4f29bcc12b4d7ebcf5a
Krunal Vala <krunalvala9@gmail.com>
2018-06-12 18:46:18 +0530

    Add Users class and updateUserList

---
+++ server/utils/users.js
@@ -0,0 +1,53 @@
+[{
+
+}]
+
+
+class Users {
+    constructor () {
+        this.users = [];
+    }
+    addUser (id, name, room) {
+        var user = {id, name, room};
+        this.users.push(user);
+        return user;
+    }
+    removeUser (id) {
+        var user = this.users.filter((user) => user.id === id)[0];
+
+        if (user) {
+            this.users = this.users.filter((user) => user.id !== id);
+        }
+        return user;
+    }
+    getUser (id) {
+        var user = this.users.filter((user) => user.id === id)[0];
+        return user;
+    }
+    getUserList (room) {
+        var users = this.users.filter((user) => user.room === room);
+        var namesArray = users.map((user) => user.name);
+
+        return namesArray;
+    }
+}
+
+module.exports = {Users};
+
+
+// class Person {
+//     constructor (name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     getUserDescription() {
+//         return `${this.name} is ${this.age} year old`;
+//     }
+// }
+
+// var me = new Person('Krunal', 26);
+// console.log('this.name', me.name);
+// console.log('this.age', me.age);
+// var description = me.getUserDescription();
+// console.log('Description', description);
